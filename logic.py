30a4112a9d2340941dea9480be691a5d44d42d18
isys35 <isys35@mail.ru>
2020-04-07 20:29:52 +0300

    получение локаций

--- logic.py
+++ logic.py
@@ -16,10 +16,15 @@ class InstaParser:
 
     def find_locations(self, location):
         url = f'https://www.instagram.com/web/search/topsearch/?context=blended&query={quote(location)}&rank_token=0&include_reel=true'
-
-
-
+        resp = self.agent.get_request(url)
+        json_resp = resp.json()
+        places = json_resp['places']
+        places_dict = {}
+        for place in places:
+            places_dict[place['place']['location']['pk']] = place['place']['location']['name']
+        return places_dict
 
 if __name__ == '__main__':
     parser = InstaParser()
     parser.auth('domimod1', 'a31081993')
+    parser.find_locations('Москва')
