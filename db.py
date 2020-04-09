8b2bbcb25904dfe59cb10e61bd0dd8e41a4e729f
isys35 <isys35@mail.ru>
2020-04-09 17:01:15 +0300

    изменение кодировки при записи файла

--- db.py
+++ db.py
@@ -2,12 +2,12 @@ import csv
 
 
 def create_file(file_name):
-    with open(file_name, "w", newline="") as file:
+    with open(file_name, "w", newline="", encoding='utf8') as file:
         csv.writer(file)
 
 
 def add_data(file_name, data):
-    with open(file_name, "a", newline="") as file:
+    with open(file_name, "a", newline="", encoding='utf8') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(data)
 
